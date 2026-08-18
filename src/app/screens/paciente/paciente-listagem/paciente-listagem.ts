42f8c4313bec911914d7c569982ec8dde2a1ffe4
Mauricio Carvalho <mauriciocarvalhoac@gmail.com>
2026-08-17 22:08:55 -0300

    849 - Listagem de paciente

--- src/app/screens/paciente/paciente-listagem/paciente-listagem.ts
+++ src/app/screens/paciente/paciente-listagem/paciente-listagem.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { AbstractComponent } from '../../abstract-component';
+import { PacienteService } from '../../../service/paciente-service';
+import { RouterLink } from '@angular/router';
+import { CpfPipe } from '../../../shared/pipes/cpf-pipe';
+import { NgxMaskDirective } from 'ngx-mask';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-paciente-listagem',
-  imports: [],
+  imports: [ReactiveFormsModule, NgxMaskDirective, CpfPipe, RouterLink],
   templateUrl: './paciente-listagem.html',
   styleUrl: './paciente-listagem.scss',
 })
-export class PacienteListagem {}
+export class PacienteListagem extends AbstractComponent implements OnInit {
+
+  private service = inject(PacienteService);
+
+  lista = signal<any>([]);
+  listaSize = signal<number>(0);
+
+  constructor() {
+    super();
+    this.formulario = this.formBuilder.group({
+      nome: null,
+      cpf: null,
+    });
+  }
+  ngOnInit(): void {
+    this.listar();
+  }
+  listar() {
+    this.service.listar().subscribe((lista: any) => {
+      this.lista.set(lista);
+      this.listaSize.set(lista.length);
+    });
+  }
+
+  limpar() {
+
+  }
+
+  filtrar() {
+
+  }
+
+  irParaEdicao(item: any) {
+
+  }
+
+  excluir(obj: any) {
+
+  }
+}
---
+++ src/app/service/paciente-service.ts
@@ -0,0 +1,45 @@
+import { Service } from '@angular/core';
+import { AbstractService } from './abstractService';
+import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+
+@Service()
+export class PacienteService extends AbstractService {
+
+    constructor() {
+        super("/pacientes");
+    }
+
+    listar(): Observable<any> {
+        return this.http.get<any>(this.url);
+    }
+
+    // filtrar(nome: string, cpf: string): Observable<any> {
+    //     var params = new HttpParams();
+
+    //     if (nome) {
+    //         params = params.set('nome', nome);
+    //     }
+    //     if (cpf) {
+    //         params = params.set('cpf', cpf);
+    //     }
+    //     return this.http.get<any>(this.url + "/filtro", { params });
+    // }
+
+    // salvar(obj: any): Observable<any> {
+    //     return this.http.post<any>(this.url, obj);
+    // }
+
+    // editar(obj: any): Observable<any> {
+    //     return this.http.put<any>(this.url + "/" + obj.id, obj);
+    // }
+
+    // excluir(id: any): Observable<any> {
+    //     return this.http.delete<any>(this.url + "/" + id);
+    // }
+
+    // buscarPorId(id: string): Observable<any> {
+    //     return this.http.get<any>(this.url + "/" + id);
+    // }
+
+}
