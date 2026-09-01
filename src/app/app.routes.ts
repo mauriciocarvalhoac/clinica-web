e2140c1cd215b9efd52c6eb1be06ac6a477b4dfb
Mauricio Carvalho <mauriciocarvalhoac@gmail.com>
2026-09-01 15:09:15 -0300

    123 - Removendo crud e service de medico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { TemplateDefault } from './core/template/template-default/template-default';
 import { DashboardGraphics } from './screens/dashboard/dashboard-graphics/dashboard-graphics';
-import { MedicoListagem } from './screens/medico/medico-listagem/medico-listagem';
-import { MedicoInclusao } from './screens/medico/medico-inclusao/medico-inclusao';
 import { Login } from './screens/login/login';
 import { authGuard } from './core/auth/auth-guard';
 import { PacienteListagem } from './screens/paciente/paciente-listagem/paciente-listagem';
@@ -25,9 +23,6 @@ export const routes: Routes = [
         path: '', component: TemplateDefault, canActivateChild: [authGuard],
         children: [
             { path: 'dashboard', component: DashboardGraphics },
-            { path: 'medico-listagem', component: MedicoListagem },
-            { path: 'medico-inclusao', component: MedicoInclusao },
-            { path: 'medico-inclusao/:id', component: MedicoInclusao },
             { path: 'funcionario-listagem', component: FuncionarioListagem },
             { path: 'funcionario-inclusao', component: FuncionarioInclusao },
             { path: 'funcionario-inclusao/:id', component: FuncionarioInclusao },
