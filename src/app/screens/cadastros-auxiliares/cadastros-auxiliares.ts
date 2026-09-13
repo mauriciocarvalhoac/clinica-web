d2bd12bf04f6c2ef2d3c025e93b62ce1c35abbb0
Mauricio Carvalho <mauriciocarvalhoac@gmail.com>
2026-09-13 20:42:53 -0300

    833 - Implementando a tab de departamento

--- src/app/screens/cadastros-auxiliares/cadastros-auxiliares.ts
+++ src/app/screens/cadastros-auxiliares/cadastros-auxiliares.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { AbstractComponent } from '../abstract-component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastros-auxiliares',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, NgbNavModule],
   templateUrl: './cadastros-auxiliares.html',
   styleUrl: './cadastros-auxiliares.scss',
 })
-export class CadastrosAuxiliares {}
+export class CadastrosAuxiliares extends AbstractComponent implements OnInit {
+  activeTab = 1;
+
+  constructor() {
+    super();
+
+  }
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      id: [null],
+    });
+  }
+
+
+}
